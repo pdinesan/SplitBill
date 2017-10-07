983eafdeece69e4180be4c3302fa351d4137ba88
pdinesan <priyakundoli@gmail.com>
2017-10-06 23:49:09 -0400

    Add project files.

---
+++ splitBill/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace splitBill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
